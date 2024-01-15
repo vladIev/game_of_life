5a18fa0b1c4804a8f08cdd4d8ee13a42ff68da43
Владислав Левоцкий <vlad.levotskiy@gmail.com>
2024-01-16 00:17:37 +0200

    Naive cell calculator

--- src/rules_engine.cpp
+++ src/rules_engine.cpp
@@ -6,17 +6,17 @@ CellCalculator::CellCalculator(const Field* field) : d_field(field) {}
 uint8_t CellCalculator::operator()(size_t x, size_t y) const
 {
     uint8_t sum1 = 0, sum2 = 0, sum3 = 0;
-
+    bool isAlive = d_field->at(x, y);
     for (int pos = std::max(static_cast<int>(x - 1), 0);
          pos <= std::min(x + 1, d_field->width() - 1);
          ++pos) {
         if (y > 0)
             sum1 += d_field->at(pos, y - 1);
         sum2 += d_field->at(pos, y);
-        if (y < d_field->height())
+        if (y < d_field->height() - 1)
             sum3 += d_field->at(pos, y + 1);
     }
     const auto sum = sum1 + sum2 + sum3;
-    return static_cast<uint8_t>(sum == 3);
+    return static_cast<uint8_t>(sum == 3 || (sum == 4 && isAlive));
 }
 } // namespace life
--- src/rules_engine.g.cpp
+++ src/rules_engine.g.cpp
@@ -7,7 +7,23 @@
 #include <vector>
 
 using namespace life;
-TEST(cell_calculator, cell_dies)
+namespace {
+auto nextGeneration(size_t width, size_t height, std::span<const uint8_t> field)
+{
+    Field f(width, height, std::span<const uint8_t>(field));
+    CellCalculator calculator(&f);
+
+    std::vector<uint8_t> result;
+    for (size_t i = 0; i < width; i++) {
+        for (size_t j = 0; j < height; j++) {
+            result.push_back(calculator(j, i));
+        }
+    }
+    return result;
+}
+} // namespace
+
+TEST(cell_calculator, cell_extincts)
 {
     // clang-format off
     const std::vector<uint8_t> field {
@@ -15,11 +31,42 @@ TEST(cell_calculator, cell_dies)
         0,1,0,
         0,0,0
     };
+    // clang-format on
+
+    Field f(3, 3, std::span<const uint8_t>(field));
+    CellCalculator calculator(&f);
+
+    EXPECT_EQ(0, calculator(1, 1));
+}
+
+TEST(cell_calculator, cell_borns)
+{
+    // clang-format off
+    const std::vector<uint8_t> field {
+        1,1,0,
+        0,0,0,
+        0,0,1
+    };
 
     const std::vector<uint8_t> expected {
         0,0,0,
-        0,0,0,
+        0,1,0,
         0,0,0
+    };
+
+    // clang-format on
+
+    const auto result = nextGeneration(3, 3, field);
+    EXPECT_EQ(result, expected);
+}
+
+TEST(cell_calculator, cell_stay_alive_with_2_neighbors)
+{
+    // clang-format off
+    const std::vector<uint8_t> field {
+        1,0,0,
+        0,1,0,
+        0,0,1
     };
 
     // clang-format on
@@ -27,12 +74,56 @@ TEST(cell_calculator, cell_dies)
     Field f(3, 3, std::span<const uint8_t>(field));
     CellCalculator calculator(&f);
 
-    std::vector<uint8_t> result;
-    for (size_t i = 0; i < 3; i++) {
-        for (size_t j = 0; j < 3; j++) {
-            result.push_back(calculator(j, i));
-        }
-    }
+    EXPECT_EQ(1, calculator(1, 1));
+}
 
-    EXPECT_EQ(result, expected);
+TEST(cell_calculator, cell_stay_alive_with_3_neighbors)
+{
+    // clang-format off
+    const std::vector<uint8_t> field {
+        1,0,1   ,
+        0,1,0,
+        0,0,1
+    };
+    // clang-format on
+
+    Field f(3, 3, std::span<const uint8_t>(field));
+    CellCalculator calculator(&f);
+
+    EXPECT_EQ(1, calculator(1, 1));
+}
+
+TEST(cell_calculator, cell_dies_starvation)
+{
+    // clang-format off
+    const std::vector<uint8_t> field {
+        1,0,1,
+        0,1,0,
+        1,0,1
+    };
+    // clang-format on
+
+    Field f(3, 3, std::span<const uint8_t>(field));
+    CellCalculator calculator(&f);
+
+    EXPECT_EQ(0, calculator(1, 1));
+}
+
+TEST(cell_calculator, borders_handling)
+{
+    // clang-format off
+    const std::vector<uint8_t> field {
+        1,0,1,
+        0,1,1,
+        1,0,1
+    };
+    // clang-format on
+
+    Field f(3, 3, std::span<const uint8_t>(field));
+    CellCalculator calculator(&f);
+
+    EXPECT_EQ(0, calculator(0, 0));
+    EXPECT_EQ(0, calculator(2, 0));
+    EXPECT_EQ(1, calculator(0, 2));
+    EXPECT_EQ(1, calculator(2, 2));
 }
